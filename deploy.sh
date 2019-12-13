e0fee7272356d77ffbcd72ede01d7c8d12162d42
Zerth Monk <26793601+zerthmonk@users.noreply.github.com>
2019-12-13 13:11:48 +0200

    deploy script

---
+++ deploy.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+
+python setup.py build sdist bdist_wheel
+version=$(awk -v FPAT="version" "NF{ print $1 }" setup.py | cut -d"'" -f2)
+
+for entry in $(ls dist | grep $version)
+do
+    curl -F package=@dist/$entry https://$FURY_TOKEN@push.fury.io/zerthmonk
+done
