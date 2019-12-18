3e58fb8b0ca52a2cf3491f0c166d34e7abc860bd
Zerth Monk <26793601+zerthmonk@users.noreply.github.com>
2019-12-18 18:27:55 +0300

    get rid of awk, thank you

--- deploy.sh
+++ deploy.sh
@@ -1,8 +1,9 @@
 #!/usr/bin/env bash
 
-source venv/bin/activate
+version=$(cat setup.py |grep version |cut -f2 -d "'")
+
+. ./venv/bin/activate
 python setup.py build sdist bdist_wheel
-version=$(awk -v FPAT="version" "NF{ print $1 }" setup.py | cut -d"'" -f2)
 
 for entry in $(ls dist | grep $version)
 do
