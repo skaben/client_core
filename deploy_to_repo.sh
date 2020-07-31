52347c406c53f9aa1c4a2ac423c069b9da59ea89
Zerth Monk <26793601+zerthmonk@users.noreply.github.com>
2020-07-31 16:14:58 +0000

    trying to add deploy

--- deploy_to_repo.sh
+++ deploy_to_repo.sh
@@ -1,19 +1,9 @@
 #!/usr/bin/env bash
 
-cd /app
-
-if [ ! -d dist ]; then
-  rm -r ./dist ./build ./build_wheel
-fi
-
 mkdir dist
-
 python setup.py build sdist
 
-# pushing to repo
-version=$(cat ./setup.py |grep version |cut -f2 -d "'")
-
-for entry in $(ls dist | grep $version)
+for entry in $(ls dist)
 do
     curl -F package=@dist/$entry https://$TOKEN@push.fury.io/zerthmonk
 done
