4befa75e3fc40673d4028b31c67217720b1f8c75
Zerth Monk <26793601+zerthmonk@users.noreply.github.com>
2020-10-16 16:48:02 +0300

    minor improvements

--- skabenclient/device.py
+++ skabenclient/device.py
@@ -12,26 +12,31 @@ class BaseDevice:
 
     config_class = DeviceConfig
 
-    def __init__(self, system_config, device_config):
-        if not isinstance(system_config, SystemConfig):
-            raise Exception(f'system_config is not a SystemConfig, but {type(system_config)} instead')
+    def __init__(self, app_config, device_config):
+        if not isinstance(app_config, SystemConfig):
+            raise Exception(f'app_config is not a SystemConfig, but {type(app_config)} instead')
         if not isinstance(device_config, self.config_class):
             raise Exception(f'device_config is not a {self.config_class}, but {type(device_config)} instead')
         # assign system config
-        self.system = system_config
-        self.q_int = system_config.get('q_int')
-        self.uid = system_config.get('uid')
-        self.logger = system_config.logger()
+        self.system = app_config
+        self.q_int = app_config.get('q_int')
+        self.uid = app_config.get('uid')
+        self.logger = app_config.logger()
         # assign device ingame config
         self.config = device_config
         self.config.load()  # load and update current running conf
 
     def run(self):
-        """ Abstract method for run device """
         print('application is starting...')
         self.logger.debug(f'device starting as: {self.system} \n {self.config}')
         reload_event = make_event('device', 'reload')
         self.q_int.put(reload_event)
+
+    def stop(self):
+        print('device is stopping...')
+        self.logger.debug(f"stopping device {self}")
+        end_event = make_event("exit", '')
+        self.q_int.put(end_event)
 
     def state_update(self, data):
         """ Update device configuration from user actions
