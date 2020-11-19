ef77edb82c1175248c7f4e306f29f56527f23b71
Zerth Monk <26793601+zerthmonk@users.noreply.github.com>
2020-11-19 20:04:47 +0200

    1.16.6

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ with open('requirements.txt', 'r') as f:
 
 setup(
     name='skabenclient',
-    version='1.16.5',
+    version='1.16.6',
     description='SKABEN client ',
     license="MIT",
     long_description=long_description,
